10e19367013f34916bc9470a10daa115cf55456d
nixmeal <arch.illusionist@gmail.com>
2012-08-16 15:02:31 +0530

    modified patches

--- dwm-6.0-patches/config.h
+++ dwm-6.0-patches/config.h
@@ -39,6 +39,8 @@ static const unsigned int borderpx  	= 1;        	/* border pixel of windows */
 static const unsigned int snap          = 2;     	// snap pixel
 static const Bool showbar               = True;  	// False means no bar
 static const Bool topbar                = True;  	// False means bottom bar
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const Bool showsystray       = True;     /* False means no systray */
 
 
 
